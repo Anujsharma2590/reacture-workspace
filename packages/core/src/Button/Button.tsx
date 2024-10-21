87ded82253e67c8fa935a4b20089d432ef0080bc
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-10-21 17:59:18 +0530

    change import

--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 export interface ButtonProps {
   label: string;
   onClick?: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+export const Button: FC<ButtonProps> = ({ label, onClick }) => {
   return <button onClick={onClick}>{label}</button>;
 };
 
