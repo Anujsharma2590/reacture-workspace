49ed806bf290fb5a718bb732486c8d3c8c4dbe4f
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-10-29 11:55:40 +0530

    remove unused code

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,4 @@
 export * from "./Button";
-// export { ReactureProvider, useTheme } from '@reacture/theme';
-// export { makeStyles } from './utils/makeStyles';
-// export { tokens } from '@reacture/theme';
-
 export { defaultTokens } from "@reacture/theme";
 export type { Tokens } from "@reacture/theme";
 export { ReactureProvider, useTheme } from "@reacture/theme";
